cb37487a854a87e327148dc7a0e12fda1275e2a4
Akash Chaturvedi <axc144430@utdallas.edu>
2016-01-23 23:32:55 -0600

    example to write model in RDF XML format on standard output

---
+++ src/tutorials/Tutorial04.java
@@ -0,0 +1,35 @@
+package tutorials;
+
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.*;
+import org.apache.log4j.varia.NullAppender;
+
+
+/** Tutorial 3 Statement attribute accessor methods
+ */
+public class Tutorial04 extends Object {
+    public static void main (String args[]) {
+    
+    	org.apache.log4j.BasicConfigurator.configure(new NullAppender());
+        // some definitions
+        String personURI    = "http://somewhere/JohnSmith";
+        String givenName    = "John";
+        String familyName   = "Smith";
+        String fullName     = givenName + " " + familyName;
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+
+        // create the resource
+        //   and add the properties cascading style
+        Resource johnSmith 
+          = model.createResource(personURI)
+                 .addProperty(VCARD.FN, fullName)
+                 .addProperty(VCARD.N, 
+                              model.createResource()
+                                   .addProperty(VCARD.Given, givenName)
+                                   .addProperty(VCARD.Family, familyName));
+        
+     // now write the model in XML form to a file
+        model.write(System.out);
+    }
+}
