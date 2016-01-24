9dc4c0b897f4cfb7f6fba61dd5ea90292e80ff3c
Akash Chaturvedi <axc144430@utdallas.edu>
2016-01-23 23:16:41 -0600

    Statement(triple) uterator demonstration code

---
+++ src/tutorials/Tutorial03.java
@@ -0,0 +1,56 @@
+package tutorials;
+
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.*;
+import org.apache.log4j.varia.NullAppender;
+
+
+/** Tutorial 3 Statement attribute accessor methods
+ */
+public class Tutorial03 extends Object {
+    public static void main (String args[]) {
+    
+    	org.apache.log4j.BasicConfigurator.configure(new NullAppender());
+        // some definitions
+        String personURI    = "http://somewhere/JohnSmith";
+        String givenName    = "John";
+        String familyName   = "Smith";
+        String fullName     = givenName + " " + familyName;
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+
+        // create the resource
+        //   and add the properties cascading style
+        Resource johnSmith 
+          = model.createResource(personURI)
+                 .addProperty(VCARD.FN, fullName)
+                 .addProperty(VCARD.N, 
+                              model.createResource()
+                                   .addProperty(VCARD.Given, givenName)
+                                   .addProperty(VCARD.Family, familyName));
+        
+        // list the statements in the graph
+        StmtIterator iter = model.listStatements();
+        
+        // print out the predicate, subject and object of each statement
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();         // get next statement
+            Resource  subject   = stmt.getSubject();   // get the subject
+            Property  predicate = stmt.getPredicate(); // get the predicate
+            RDFNode   object    = stmt.getObject();    // get the object
+            
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            
+            if (object instanceof Resource) {
+                System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+            
+            System.out.println(" .");
+        }
+    }
+}
+
